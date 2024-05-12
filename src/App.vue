70126d2ab4358aa1e4c6f1e729af6269ac0ad4ac
nikoladomazetovikj <nikolad150@gmail.com>
2024-05-12 02:00:39 +0200

    initial chat setup

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,85 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, reactive, watchEffect } from 'vue';
+import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
+
+const messages = ref([]);
+const newMessage = ref('');
+const conversationId = import.meta.env.VITE_CHAT_ID;
+
+window.Pusher = Pusher;
+const echo = new Echo({
+  broadcaster: 'reverb',
+  key: import.meta.env.VITE_REVERB_APP_KEY,
+  wsHost: import.meta.env.VITE_REVERB_HOST,
+  wsPort: import.meta.env.VITE_REVERB_PORT,
+  wssPort: import.meta.env.VITE_REVERB_PORT,
+  forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+  enabledTransports: ['ws', 'wss'],
+  authEndpoint: `${import.meta.env.VITE_BASE_URL}/api/broadcasting/auth`,
+  auth: {
+    headers: {
+      Authorization: `Bearer ${import.meta.env.VITE_BEARER}`,
+      Accept: 'application/json',
+    },
+  },
+});
+
+const messagesChannel = echo.private(conversationId);
+
+watchEffect(() => {
+  const handleNewMessage = (data) => {
+    console.log(data)
+  };
+
+  messagesChannel.listen('SendMessage', (e) => {
+    handleNewMessage(e);
+  });
+
+  return () => {
+    messagesChannel.stopListening('SendMessage', handleNewMessage);
+  };
+});
+
+const sendMessage = () => {
+  if (newMessage.value.trim() !== '') {
+    const messageData = {
+      sender_id: 1,
+      users: {
+        user1: {
+          name: "Test",
+          id: 1,
+          avatar: "img",
+          role: "Admin"
+        },
+        user2: {
+          name: "Test",
+          id: 2,
+          avatar: "img",
+          role: "Employee"
+        },
+      },
+      message: newMessage.value,
+      channel_name: conversationId,
+    };
+
+    echo.connector.pusher.send_event('SendMessage', JSON.stringify({ messageData }),  conversationId);
+    newMessage.value = '';
+  }
+};
 </script>
+
+
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <ul>
+      <li v-for="(message, index) in messages" :key="index">
+        <strong>{{ message.senderName }} {{ message.senderSurname }}:</strong> {{ message.message }}
+      </li>
+    </ul>
+    <input type="text" v-model="newMessage" />
+    <button @click="sendMessage">Send</button>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
